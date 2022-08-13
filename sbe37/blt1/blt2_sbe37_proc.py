212d5a0d2bbc0a4ea3e7054080bdcec06245fcdd
Gunnar Voet <gvoet@ucsd.edu>
2022-08-13 11:25:04 +0100

    sbe37 processing

--- sbe37/blt1/blt2_sbe37_proc.py
+++ sbe37/blt1/blt2_sbe37_proc.py
@@ -37,7 +37,7 @@ plt.ion()
 
 
 # %% [markdown]
-# # BLT1 SBE37 data processing
+# # BLT2 SBE37 data processing
 
 # %% [markdown]
 # ## Mooring locations
@@ -72,7 +72,7 @@ def construct_37_paths(sn, mooring):
 # ## Read time offsets
 
 # %%
-offset_file = blt_data.joinpath('blt1_SBE37_time_offsets.txt')
+offset_file = blt_data.joinpath('blt2_SBE37_time_offsets.txt')
 
 # %%
 time_offsets = pd.read_csv(offset_file, engine='python', header=0, delim_whitespace=True, parse_dates={'utc': [3, 4], 'inst': [1, 2]}, index_col='SN')
@@ -97,11 +97,11 @@ meta = dict(
 # %% [markdown]
 # ### 12709
 #
-# MAVS1
+# MAVS4
 
 # %%
 sn = 12709
-mooring = 'MAVS1'
+mooring = 'MAVS4'
 mloc = loc.sel(mooring=mooring)
 
 # %%
@@ -123,35 +123,22 @@ cuttime = None
 sn12709 = sbe.sbe37.proc(raw_file, data_out=data_out, figure_out=fig_out, lat=lat, lon=lon, meta=meta)
 
 # %% [markdown]
-# Shortly before data drop out the clock becomes unstable:
+# The microcats were reset after the CTD calibration casts and started sampling again on Oct 17. Read again starting at that date to avoid the gap. Nevermind, let's cut to the times when the instrument was at depth.
 
 # %%
-cutoff = sbe.sbe37.clock_check(sn12709)
+cut_beg = np.datetime64('2021-10-17 00:00:00')
 
 # %%
-fig, ax = gv.plot.quickfig()
-sn12709.sel(time=slice('2021-08-20 02:00', '2021-08-22 07:00')).p.plot(linewidth=0.75);
-sn12709.sel(time=slice('2021-08-20 02:00', cutoff)).p.plot(linewidth=0.75);
-
-# %% [markdown]
-# Read again with cutoff time, apply clock offset, cut beginning of time series, and save as final.
+sn12709.p.sel(time=slice('2021-10-22 15:00', '2021-10-22 17:00')).plot()
 
 # %%
-fig, ax = gv.plot.quickfig()
-sn12709.sel(time=slice('2021-07-06 14:15', '2021-07-06 14:35')).p.plot(linewidth=0.75, marker='o');
+cut_beg = np.datetime64('2021-10-22 16:00:00')
 
 # %%
-fig, ax = gv.plot.quickfig()
-sn12709.sel(time=slice('2021-07-06 14:28', '2021-07-06 14:31')).p.plot(linewidth=0.75, marker='o');
-
-# %% [markdown]
-# Set time for cut netcdf file.
+sn12709.p.sel(time=slice('2022-08-08 11:00', '2022-08-08 12:00')).plot()
 
 # %%
-cut_beg = np.datetime64('2021-07-06 14:30:00')
-
-# %% [markdown]
-# Set name for cut netcdf file.
+cut_end = np.datetime64('2022-08-08 11:30:00')
 
 # %%
 file_name = f'BLT1_{mooring}_SBE37_SN{sn}.nc'
@@ -163,10 +150,7 @@ file_name[:file_name.find('.nc')]
 # Re-read the raw data, this time cut at end and beginning and apply the time offset.
 
 # %%
-sn12709 = sbe.sbe37.proc(raw_file, insttime, utctime, data_out=data_out, file_name=file_name, figure_out=fig_out, cut_end=cutoff, cut_beg=cut_beg, lat=lat, lon=lon, meta=meta)
-
-# %% [markdown]
-# Now the clock  check should fail:
+sn12709 = sbe.sbe37.proc(raw_file, insttime, utctime, data_out=data_out, file_name=file_name, figure_out=fig_out, cut_end=cut_end, cut_beg=cut_beg, lat=lat, lon=lon, meta=meta)
 
 # %%
 sbe.sbe37.clock_check(sn12709)
@@ -174,11 +158,11 @@ sbe.sbe37.clock_check(sn12709)
 # %% [markdown]
 # ### 12710
 #
-# MAVS2
+# MAVS4
 
 # %%
 sn = 12710
-mooring = 'MAVS2'
+mooring = 'MAVS4'
 mloc = loc.sel(mooring=mooring)
 
 # %%
@@ -198,37 +182,13 @@ lat = mloc.lat_actual.data
 mcraw = sbe.sbe37.proc(raw_file, data_out=data_out, figure_out=fig_out, lat=lat, lon=lon, meta=meta)
 
 # %% [markdown]
-# Process and save the whole time series without cutting or applying the time offset.
-
-# %% [markdown]
-# Shortly before data drop out the clock becomes unstable:
-
-# %%
-cutoff = sbe.sbe37.clock_check(mcraw)
-
-# %%
-t0 = cutoff - np.timedelta64(1, 'D')
-t1 = cutoff + np.timedelta64(1, 'D')
-fig, ax = gv.plot.quickfig()
-mcraw.sel(time=slice(t0, t1)).p.plot(linewidth=0.75);
-mcraw.sel(time=slice(t0, cutoff)).p.plot(linewidth=0.75);
-
-# %%
-fig, ax = gv.plot.quickfig()
-mcraw.sel(time=slice('2021-07-07 13:31', '2021-07-07 13:42')).p.plot(linewidth=0.75, marker='o');
-
-# %%
-fig, ax = gv.plot.quickfig()
-mcraw.sel(time=slice('2021-07-07 13:41', '2021-07-07 14:00')).p.plot(linewidth=0.75, marker='o');
-
-# %% [markdown]
 # Set time for cut netcdf file.
 
 # %%
-cut_beg = np.datetime64('2021-07-07 13:50:00')
+cut_beg = np.datetime64('2021-10-22 16:00:00')
 
-# %% [markdown]
-# Set name for cut netcdf file.
+# %%
+cut_end = np.datetime64('2022-08-08 11:30:00')
 
 # %%
 file_name = f'BLT1_{mooring}_SBE37_SN{sn}.nc'
@@ -240,10 +200,7 @@ file_name[:file_name.find('.nc')]
 # Re-read the raw data, this time cut at end and beginning and apply the time offset.
 
 # %%
-mc = sbe.sbe37.proc(raw_file, insttime, utctime, data_out=data_out, file_name=file_name, figure_out=fig_out, cut_end=cutoff, cut_beg=cut_beg, lat=lat, lon=lon, meta=meta)
-
-# %% [markdown]
-# Now the clock  check should fail:
+mc = sbe.sbe37.proc(raw_file, insttime, utctime, data_out=data_out, file_name=file_name, figure_out=fig_out, cut_end=cut_end, cut_beg=cut_beg, lat=lat, lon=lon, meta=meta)
 
 # %%
 sbe.sbe37.clock_check(mc)
@@ -251,12 +208,15 @@ sbe.sbe37.clock_check(mc)
 # %% [markdown]
 # ### 12711
 #
-# MAVS2
+# MAVS3
 
 # %%
 sn = 12711
-mooring = 'MAVS2'
+mooring = 'MAVS3'
 mloc = loc.sel(mooring=mooring)
+
+# %%
+meta['mooring'] = mooring
 
 # %% [markdown]
 # Process and save the whole time series without cutting or applying the time offset.
@@ -274,31 +234,19 @@ cuttime = None
 mcraw = sbe.sbe37.proc(raw_file, data_out=data_out, figure_out=fig_out, cut_end=cuttime, lat=lat, lon=lon)
 
 # %% [markdown]
-# Shortly before data drop out the clock becomes unstable:
-
-# %%
-cutoff = sbe.sbe37.clock_check(mcraw, timedelta=200)
-
-# %%
-t0 = cutoff - np.timedelta64(12, 'h')
-t1 = cutoff + np.timedelta64(12, 'h')
-fig, ax = gv.plot.quickfig()
-mcraw.sel(time=slice(t0, t1)).p.plot(linewidth=0.75);
-mcraw.sel(time=slice(t0, cutoff)).p.plot(linewidth=0.75);
-
-# %%
-fig, ax = gv.plot.quickfig()
-mcraw.sel(time=slice('2021-07-07 13:31', '2021-07-07 13:42')).p.plot(linewidth=0.75, marker='o');
-
-# %%
-fig, ax = gv.plot.quickfig()
-mcraw.sel(time=slice('2021-07-07 13:41', '2021-07-07 14:00')).p.plot(linewidth=0.75, marker='o');
-
-# %% [markdown]
 # Set time for cut netcdf file.
 
 # %%
-cut_beg = np.datetime64('2021-07-07 13:50:00')
+mcraw.p.sel(time=slice('2021-10-22 20:30', '2021-10-22 22:00')).plot()
+
+# %%
+cut_beg = np.datetime64('2021-10-22 21:00:00')
+
+# %%
+mcraw.p.sel(time=slice('2022-08-04 13:00', '2022-08-04 16:00')).plot()
+
+# %%
+cut_end = np.datetime64('2022-08-04 13:30:00')
 
 # %% [markdown]
 # Set name for cut netcdf file.
@@ -313,13 +261,7 @@ file_name[:file_name.find('.nc')]
 # Re-read the raw data, this time cut at end and beginning and apply the time offset.
 
 # %%
-meta['mooring'] = mooring
-
-# %%
-mc = sbe.sbe37.proc(raw_file, insttime, utctime, data_out=data_out, file_name=file_name, figure_out=fig_out, cut_end=cutoff, cut_beg=cut_beg, lat=lat, lon=lon, meta=meta)
-
-# %% [markdown]
-# Now the clock  check should fail:
+mc = sbe.sbe37.proc(raw_file, insttime, utctime, data_out=data_out, file_name=file_name, figure_out=fig_out, cut_end=cut_end, cut_beg=cut_beg, lat=lat, lon=lon, meta=meta)
 
 # %%
 sbe.sbe37.clock_check(mc)
